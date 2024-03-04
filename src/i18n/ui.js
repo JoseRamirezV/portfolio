e61066c7aa92f2a21fbb22abfd774b34693f26b9
JoseRamirez25 <jr.ramirez.varon@gmail.com>
2024-03-04 12:22:25 -0500

    ADD i18n (en) FIX available for work link hover, some icons colors, skill card gradient on mobile

---
+++ src/i18n/ui.js
@@ -0,0 +1,25 @@
+export const languages = {
+  en: "En",
+  es: "Es",
+};
+
+export const defaultLang = "es";
+
+export const ui = {
+  es: {
+    "nav.experience": "Experiencia",
+    "nav.projects": "Proyectos",
+    "nav.skills": "Conocimientos",
+    "nav.aboutMe": "Sobre mi",
+    "nav.contact": "Contacto"
+  },
+  en: {
+    "nav.experience": "Experience",
+    "nav.projects": "Projects",
+    "nav.skills": "Skills",
+    "nav.aboutMe": "About me",
+    "nav.contact": "Contact"
+  },
+};
+
+export const showDefaultLang = false;
