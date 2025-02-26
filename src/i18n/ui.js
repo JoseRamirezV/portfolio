87836761abe83d48b124e915cf9b9b43fc5b5163
JoseRamirezV <jr.ramirez.varon@gmail.com>
2025-02-25 21:45:30 -0500

    UPDATE resume info. ADD Props interface to icons, minor changes

--- src/i18n/ui.js
+++ src/i18n/ui.js
@@ -12,7 +12,10 @@ export const ui = {
     "nav.skills": "Habilidades",
     "nav.aboutMe": "Sobre mi",
     "nav.contact": "Contacto",
-    "nav.colorMode": "Tema",
+    "theme.colorMode": "Tema",
+    "theme.darkMode": "Oscuro",
+    "theme.lightMode": "Claro",
+    "theme.system": "Sistema",
     "footer.basedOn": "Inspirado por",
     "hero.available": "Disponible para trabajar",
     "hero.intro": "Hola! soy Ricardo Ramirez"
@@ -23,7 +26,10 @@ export const ui = {
     "nav.skills": "Skills",
     "nav.aboutMe": "About me",
     "nav.contact": "Contact",
-    "nav.colorMode": "Theme",
+    "theme.colorMode": "Theme",
+    "theme.darkMode": "Dark",
+    "theme.lightMode": "Light",
+    "theme.system": "System",
     "footer.basedOn": "Inspired by",
     "hero.available": "Available for work",
     "hero.intro": "Hey! I'm Ricardo Ramirez"
